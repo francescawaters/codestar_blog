b4175f3b28a5f2450da2662ea81f895625f94f6c
francescawaters <francescawaters26@outlook.com>
2025-02-21 14:52:15 +0000

    Add Procfile for deployment and update settings for production

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ng%v&=%#&uit4x=v)yji@mp=q)+lsd+!^0oyk6&l1*lovo3j!5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [,'.herokuapp.com']
 
 
 # Application definition
