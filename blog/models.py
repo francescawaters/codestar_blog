043084f132eb4cf2dad5a8788ca322da84996750
francescawaters <francescawaters26@outlook.com>
2025-02-28 14:05:13 +0000

    Add About app with model, admin, views, and templates

--- blog/models.py
+++ blog/models.py
@@ -22,6 +22,7 @@ class Post(models.Model):
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
+
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name='comments')
