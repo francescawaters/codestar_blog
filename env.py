0862cd9d1de0d01a0442573fc3a47393049a6ff3
francescawaters <francescawaters26@outlook.com>
2025-03-03 17:27:07 +0000

    Add featured image support to posts with Cloudinary integration

--- env.py
+++ env.py
@@ -4,3 +4,5 @@ os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_E63uxhPUTwep@ep-tight-sun-a2r9okq7.eu-central-1.aws.neon.tech/outer_tusk_flop_322319")
 os.environ.setdefault(
     "SECRET_KEY", "30n#4bo=#v1vte!gkw1olrqg(fh27pp3!%khi3(itb(^41e=qp")
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://543797754382734:8AwS01-dFi-L_eVhGODA8edvTmU@dgy8pc494")
