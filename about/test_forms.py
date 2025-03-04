59437c8a00908d9dda16ff1998e8c3e61d7948c0
francescawaters <francescawaters26@outlook.com>
2025-03-04 13:27:13 +0000

    Add tests for CollaborateForm and CommentForm in about and blog views

--- about/test_forms.py
+++ about/test_forms.py
@@ -4,8 +4,14 @@ from .forms import CollaborateForm
 
 class TestCollaborateForm(TestCase):
 
-    def test_form_is_valid …
-        …
+    def test_form_is_valid(self):
+        """ Test for all fields"""
+        form = CollaborateForm({
+            'name': '',
+            'email': 'test@test.com',
+            'message': 'Hello!'
+        })
+        self.assertTrue(form.is_valid(), msg="Form is not valid")
 
     def test_name_is_required(self):
         """Test for the 'name' field"""
